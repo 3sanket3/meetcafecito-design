792e3846b18bc9794f9fe2891ff82f4d038b1cb7
Sanket Patel <3sanket3@gmail.com>
2020-04-20 21:04:34 +0530

    add video and control bar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,103 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import logo from "./logo.png";
+
+import "./App.css";
+import styled from "styled-components";
+import { FaVolumeUp, FaShareSquare, FaVideo } from "react-icons/fa";
+
+const Layout = styled.div`
+  padding: 0 5%;
+`;
+const Header = styled.div`
+  text-align: center;
+  padding: 15px;
+  margin-bottom: 15px;
+`;
+
+const NavigationContainer = styled.div`
+  margin-bottom: 15px;
+`;
+
+const CallContainer = styled.div`
+  display: grid;
+  grid-template-columns: auto 20%;
+  border: 8px solid #f8f5f5;
+  box-sizing: border-box;
+  border-radius: 5px;
+`;
+
+const VideoScreensContainer = styled.div``;
+const VideoTileContainer = styled.div`
+  display: grid;
+  grid-template-columns: 30% 30%;
+  grid-gap: 10px;
+  justify-content: center;
+  margin: 15px;
+`;
+
+const AgendaContainer = styled.div`
+  border-left: 8px solid #f8f5f5;
+`;
+
+const Video = styled.div`
+  padding-top: 56%;
+
+  position: relative;
+  background: #ccc;
+`;
+
+const ControlWrapper = styled.div`
+  width: 100%;
+  display: flex;
+  height: 60px;
+  background: #000;
+`;
+const Control = styled.div`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  font-size: 2em;
+  padding: 10px;
+  margin: 5px;
+  color: #fff;
+  border-radius: 5px;
+  :hover {
+    background: #ccc;
+  }
+`;
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
+    <Layout>
+      <Header>
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      </Header>
+      <NavigationContainer>
+        <a href="#"> Go back to home</a>
+      </NavigationContainer>
+
+      <CallContainer>
+        <VideoScreensContainer>
+          <VideoTileContainer>
+            <Video></Video>
+            <Video></Video>
+            <Video></Video>
+          </VideoTileContainer>
+          <ControlWrapper>
+            <Control>
+              <FaVolumeUp />{" "}
+            </Control>
+            <Control>
+              <FaVideo />
+            </Control>
+            <Control>
+              <FaShareSquare />
+            </Control>
+          </ControlWrapper>
+        </VideoScreensContainer>
+        <AgendaContainer></AgendaContainer>
+      </CallContainer>
+    </Layout>
   );
 }
 
